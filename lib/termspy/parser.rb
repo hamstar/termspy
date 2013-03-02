612382ca90a2990ac9cdec3fac1b22df4269ff1c
Robert <hamstar@telescum.co.nz>
2013-03-02 22:00:39 +1300

    added some parser examples

--- lib/termspy/parser.rb
+++ lib/termspy/parser.rb
@@ -1,5 +1,17 @@
 module Termspy
   class Parser
+    def initialize(body)
+      @body = body
+    end
 
+    def parse
+      #@body = Pandoc::HTML.to_markdown(@body)
+      preprocess
+      postprocess
+      @body
+    end
+
+    def preprocess; end
+    def postprocess; end
   end
 end
---
+++ lib/termspy/parsers/reddit_user_agreement.rb
@@ -0,0 +1,21 @@
+require 'termpy/parser'
+
+module Termspy
+  module Parsers
+    class RedditUserAgreement < Parser
+
+      def site
+        :reddit
+      end
+
+      def preprocess
+
+      end
+
+      def postprocess
+
+      end
+
+    end
+  end
+end
